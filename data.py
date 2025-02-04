b8e7f98c0815a79e99beda5c388097f78435ffee
Ayaz Ali <ayazalilakho2@gmail.com>
2025-02-04 15:30:25 +0500

    Update data.py

--- data.py
+++ data.py
@@ -1,18 +1,16 @@
 # data.py
 
 question_data = [
-    {"text": "A slug's blood is green.", "answer": "True"},
-    {"text": "The loudest animal is the African Elephant.", "answer": "False"},
-    {"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
-    {"text": "The total surface area of a human lungs is the size of a football pitch.", "answer": "True"},
-    {"text": "In West Virginia, USA, if you accidentally hit an animal with your car, you are free " 
-    "to take it home to eat.", "answer": "True"},
-    {"text": "In London, UK, if you happen to die in the House of Parliament, you are" 
-     "entitled to a state funeral.", "answer": "False"},
-    {"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
-    {"text": "You can lead a cow down stairs but not up stairs.", "answer": "False"},
-    {"text": "Google was originally called 'Backrub'.", "answer": "True"},
-    {"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
-    {"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
-    {"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
+    {"text": "In machine learning, supervised learning requires labeled data.", "answer": "True"},
+    {"text": "In unsupervised learning, the algorithm learns from labeled data.", "answer": "False"},
+    {"text": "Linear regression aims to find a line that best fits the data.", "answer": "True"},
+    {"text": "The cost function in linear regression is used to minimize the error between predicted and actual values.", "answer": "True"},
+    {"text": "In machine learning, the term 'overfitting' means that the model performs well on both training and test data.", "answer": "False"},
+    {"text": "The gradient descent algorithm is used to minimize the cost function in linear regression.", "answer": "True"},
+    {"text": "In unsupervised learning, the algorithm tries to predict outputs from inputs.", "answer": "False"},
+    {"text": "The cost function in linear regression is also known as Mean Squared Error (MSE).", "answer": "True"},
+    {"text": "A decision tree is a type of supervised learning algorithm.", "answer": "True"},
+    {"text": "In reinforcement learning, the algorithm learns from labeled data and predictions.", "answer": "False"},
+    {"text": "Logistic regression is used for classification tasks, not for regression tasks.", "answer": "True"},
+    {"text": "K-means clustering is a type of supervised learning algorithm.", "answer": "False"}
 ]
